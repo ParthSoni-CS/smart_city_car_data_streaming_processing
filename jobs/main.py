d9a9436a29c41e377dad29bae316b8843b5afc3b
ParthSoni-CS <parthsoni08072000@gmail.com>
2025-02-10 17:05:36 -0600

    created methods for generating synthetic data for weather gps and emergency

--- jobs/main.py
+++ jobs/main.py
@@ -28,6 +28,8 @@ TRAFFIC_TOPIC = os.getenv('TRAFFIC_TOPIC', 'traffic_data')
 WEATHER_TOPIC = os.getenv('WEATHER_TOPIC', 'weather_data')
 EMERGENCY_TOPIC = os.getenv('EMERGENCY_TOPIC', 'emergency_data')
 
+random.seed(42)
+
 start_time = datetime.now()
 
 start_location = LONDON_COORDINATES.copy()
@@ -37,6 +39,33 @@ def get_next_time():
     start_time += timedelta(seconds=random.randint(30,60))
     return start_time
 
+def generate_emergency_incident_data(device_id, timestamp, location):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id,
+        'incidentId': uuid.uuid4(),
+        'type': random.choice(['Accident', 'Fire', 'Medical', 'Police', 'None']),
+        'timestamp': timestamp,
+        'location': location,
+        'status': random.choice(['Active', 'Resolved']),
+        'description': 'Description of the incident'
+
+    }
+    
+
+def generate_weather_data(device_id, timestamp, location):
+    return {
+        'id': uuid.uuid4(),
+        'deviceId': device_id, 
+        'location': location, 
+        'timestamp': timestamp, 
+        'temperature': random.uniform(-5, 26),
+        'weatherCondition': random.choice(['Sunny', 'Cloudy', 'Rain', 'Snow']), 
+        'precipitation': random.uniform(0, 25), 
+        'windSpeed': random.uniform(0, 100), 
+        'humidity': random.uniform(0, 100), 
+        'airQualityIndex': random.uniform(0, 500)
+    }
 def generate_gps_data(device_id, timestamp, vehicle_type='private'):
     return {
         'id': uuid.uuid4(),
@@ -47,12 +76,13 @@ def generate_gps_data(device_id, timestamp, vehicle_type='private'):
         'vehicle_type': vehicle_type
     }
 
-def generate_traffic_camera_data(device_id, timestamp, camera_id):
+def generate_traffic_camera_data(device_id, timestamp, location, camera_id):
     return {
         'id': uuid.uuid4(),
         'deviceId': device_id,
         'cameraId': camera_id,
         'timestamp': timestamp,
+        'location': location,
         'snapshot': 'Base64EncodedString'
     }
 
@@ -88,8 +118,17 @@ def generate_vehicle_data(vehicle_id):
 def simulate_journey(producer, vehicle_id):
     while True:
         vehicle_data = generate_vehicle_data(vehicle_id)
-        gps_data = generate_gps_data(vehicle_id, vehicle_id['timestamp'])
-        traffic_camera_data = generate_traffic_camera_data(vehicle_id, vehicle_data['timestamp'], 'Canon200D')
+        gps_data = generate_gps_data(vehicle_id, vehicle_data['timestamp'])
+        traffic_camera_data = generate_traffic_camera_data(vehicle_id, vehicle_data['timestamp'], vehicle_data['location'],'Canon200D')
+        weather_data = generate_weather_data(vehicle_id, vehicle_data['timestamp'], vehicle_data['location'])
+        emergency_incident_data = generate_emergency_incident_data(vehicle_id, vehicle_data['timestamp'], vehicle_data['location'])
+
+        print("Vehicle data : ", vehicle_data)
+        print("Gps data : ", gps_data)
+        print("traffic camera data : ", traffic_camera_data)
+        print("weather data : ",weather_data)
+        print("emergency incident data : ", emergency_incident_data)
+
         break
 
 if __name__ == '__main__':
